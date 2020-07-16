24b13cca86703c5c0bb7de5a11ec165e5b1a87c6
DomDomo <g.domis02@gmail.com>
2020-07-16 17:14:16 +0300

    Finish 1.7

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
 
-const Rating = (props) => (
+const Statistic = (props) => (
   <p>
     {props.text} {props.value}
   </p>
@@ -19,6 +19,11 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const totalReviews = good + neutral + bad;
+  const averageReviews = totalReviews ? (good - bad) / totalReviews : 0;
+  const positive = totalReviews ? (good / totalReviews) * 100 : 0;
+  const positiveReviews = `${positive}%`;
+
   return (
     <div>
       <Header text={"give feedback"} />
@@ -26,9 +31,12 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Header text={"statistics"} />
-      <Rating text={"good"} value={good} />
-      <Rating text={"neutral"} value={neutral} />
-      <Rating text={"bad"} value={bad} />
+      <Statistic text={"good"} value={good} />
+      <Statistic text={"neutral"} value={neutral} />
+      <Statistic text={"bad"} value={bad} />
+      <Statistic text={"all"} value={totalReviews} />
+      <Statistic text={"average"} value={averageReviews} />
+      <Statistic text={"positive"} value={positiveReviews} />
     </div>
   );
 };
