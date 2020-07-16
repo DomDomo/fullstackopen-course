1ab260a797f97bbbf5c2ad920900669a26b43183
DomDomo <g.domis02@gmail.com>
2020-07-16 18:14:52 +0300

    Finish 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -13,30 +13,75 @@ const Statistic = (props) => (
   </p>
 );
 
+const Statistics = (props) => {
+  const components = [];
+
+  props.feedback.forEach((statistic) => {
+    components.push(
+      <Statistic text={statistic.text} value={statistic.value} />
+    );
+  });
+
+  return <div>{components}</div>;
+};
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const totalReviews = good + neutral + bad;
-  const averageReviews = totalReviews ? (good - bad) / totalReviews : 0;
-  const positive = totalReviews ? (good / totalReviews) * 100 : 0;
-  const positiveReviews = `${positive}%`;
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  const all = good + neutral + bad;
+  const average = all ? (good - bad) / all : 0;
+  const positive = `${all ? (good / all) * 100 : 0} %`;
+
+  const feedback = [
+    {
+      text: "good",
+      value: good,
+    },
+    {
+      text: "neutral",
+      value: neutral,
+    },
+    {
+      text: "bad",
+      value: bad,
+    },
+    {
+      text: "all",
+      value: all,
+    },
+    {
+      text: "average",
+      value: average,
+    },
+    {
+      text: "positive",
+      value: positive,
+    },
+  ];
 
   return (
     <div>
       <Header text={"give feedback"} />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <Header text={"statistics"} />
-      <Statistic text={"good"} value={good} />
-      <Statistic text={"neutral"} value={neutral} />
-      <Statistic text={"bad"} value={bad} />
-      <Statistic text={"all"} value={totalReviews} />
-      <Statistic text={"average"} value={averageReviews} />
-      <Statistic text={"positive"} value={positiveReviews} />
+      <Statistics feedback={feedback} />
     </div>
   );
 };
