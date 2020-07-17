ddc64d8d2983680ee481b9c39df50e17bf05b417
DomDomo <g.domis02@gmail.com>
2020-07-17 17:06:14 +0300

    Finish 2.3

--- part2/course_information/src/index.js
+++ part2/course_information/src/index.js
@@ -13,14 +13,20 @@ const Part = (props) => {
   );
 };
 
-const Content = ({course}) => {
+const Content = ({ course }) => {
   return (
     <div>
-      {course.parts.map((part) =>
-        <Part key={part.id} part={part}/>
-      )}
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
     </div>
   );
+};
+
+const Total = ({ course }) => {
+  let total = course.parts.reduce((accum, part) => accum + part.exercises, 0);
+
+  return <b>total of {total} exercises</b>;
 };
 
 const Course = ({ course }) => {
@@ -28,34 +34,40 @@ const Course = ({ course }) => {
     <div>
       <Header course={course} />
       <Content course={course} />
+      <Total course={course} />
     </div>
   );
-}
+};
 
 const App = () => {
   const course = {
     id: 1,
-    name: 'Half Stack application development',
+    name: "Half Stack application development",
     parts: [
       {
-        name: 'Fundamentals of React',
+        name: "Fundamentals of React",
         exercises: 10,
-        id: 1
+        id: 1,
       },
       {
-        name: 'Using props to pass data',
+        name: "Using props to pass data",
         exercises: 7,
-        id: 2
+        id: 2,
       },
       {
-        name: 'State of a component',
+        name: "State of a component",
         exercises: 14,
-        id: 3
+        id: 3,
       },
-    ]
-  }
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
+      },
+    ],
+  };
 
-  return <Course course={course} />
-}
+  return <Course course={course} />;
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
