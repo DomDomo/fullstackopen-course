6ee30f394577afb02cf8a149e16b825ccdea366c
DomDomo <g.domis02@gmail.com>
2020-07-17 17:19:16 +0300

    Finish 2.5

--- part2/course_information/src/index.js
+++ part2/course_information/src/index.js
@@ -1,43 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ course }) => {
-  let total = course.parts.reduce((accum, part) => accum + part.exercises, 0);
-
-  return <b>total of {total} exercises</b>;
-};
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
