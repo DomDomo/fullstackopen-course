a2ff759062320617168e8e16e9aa082756e32acb
DomDomo <g.domis02@gmail.com>
2021-01-17 07:35:38 +0200

    Actually add build

---
+++ build/precache-manifest.f1384ead7b68b77d7bce0c0911776549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a84a66b3c35c2d6842627b0a03b0954",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6e1c508e269c40927c9",
+    "url": "/static/css/main.c6e80131.chunk.css"
+  },
+  {
+    "revision": "42727bb67226b4abe3d5",
+    "url": "/static/js/2.229d9b9c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.229d9b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e1c508e269c40927c9",
+    "url": "/static/js/main.b7c636ef.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
