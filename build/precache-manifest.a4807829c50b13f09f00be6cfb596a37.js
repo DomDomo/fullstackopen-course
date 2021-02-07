be1f93846762e2af7368ce8e917913e44924dbf4
DomDomo <g.domis02@gmail.com>
2021-02-07 06:16:55 +0200

    Change gitignore

---
+++ build/precache-manifest.a4807829c50b13f09f00be6cfb596a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3566b5a18ec74d4aac31ac7ca31dad20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c1194548ae066c9bb61",
+    "url": "/static/css/main.c6e80131.chunk.css"
+  },
+  {
+    "revision": "42727bb67226b4abe3d5",
+    "url": "/static/js/2.229d9b9c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.229d9b9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c1194548ae066c9bb61",
+    "url": "/static/js/main.a98b2cfc.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
