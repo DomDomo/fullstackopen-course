e9ac1a49104974a4b198023397f2727fcc4dea70
DomDomo <g.domis02@gmail.com>
2021-02-10 23:33:00 +0200

    Finish 3.22

--- index.js
+++ index.js
@@ -9,102 +9,102 @@ app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
-morgan.token("body", req => JSON.stringify(req.body));
+morgan.token('body', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
-    response.send('<h1>Welcom to the phonebook database</h1>')
+	response.send('<h1>Welcom to the phonebook database</h1>')
 })
 
 app.get('/info', (request, response, next) => {
-    Person.countDocuments({})
-    .then(peopleNum => {
-        const serverTime = new Date()
-        const data = `<h1>Phonebook has info for ${peopleNum} people</h1>
+	Person.countDocuments({})
+		.then(peopleNum => {
+			const serverTime = new Date()
+			const data = `<h1>Phonebook has info for ${peopleNum} people</h1>
         <h2>${serverTime}</h2>`
-        response.send(data)
-    })
-    .catch(error => next(error))
+			response.send(data)
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response, next) => {
-    Person.find({})
-    .then(people => {
-        response.json(people)
-    })
-    .catch(error => next(error))
+	Person.find({})
+		.then(people => {
+			response.json(people)
+		})
+		.catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-    .then(person => {
-        response.json(person)
-    })
-    .catch(error => next(error))
+	Person.findById(request.params.id)
+		.then(person => {
+			response.json(person)
+		})
+		.catch(error => next(error))
 })
 
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+	Person.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
-    
-    const body = request.body
 
-    if (body.name === undefined) {
-        return response.status(400).json({ error: 'content missing' })
-    }
+	const body = request.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+	if (body.name === undefined) {
+		return response.status(400).json({ error: 'content missing' })
+	}
 
-    person.save()
-    .then(savedPerson => {
-        response.json(savedPerson)
-    })
-    .catch(error => next(error))
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
+
+	person.save()
+		.then(savedPerson => {
+			response.json(savedPerson)
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    Person
-    .findOneAndUpdate(
-        {_id: request.params.id},
-        {number: request.body.number},
-        {returnOriginal: false, runValidators: true}
-    )
-    .then((updatedPerson)=>{
-        response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+	Person
+		.findOneAndUpdate(
+			{ _id: request.params.id },
+			{ number: request.body.number },
+			{ returnOriginal: false, runValidators: true }
+		)
+		.then((updatedPerson) => {
+			response.json(updatedPerson)
+		})
+		.catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-    if (error.name === 'CastError' && error.kind == 'ObjectId') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+	console.error(error.message)
+	if (error.name === 'CastError' && error.kind === 'ObjectId') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+	if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
+	next(error)
 }
 app.use(errorHandler)
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
 app.use(unknownEndpoint)
 
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -5,36 +5,36 @@ const url = process.env.MONGODB_URI
 console.log('Connecting to Phonebook server')
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(result => {
-    console.log('Connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+	.then(() => {
+		console.log('Connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: [3,'Name must have atleast 3 characters'],
-    required: true,
-    unique: true,
-  },
-  number: {
-    minlength: [8,'Number must have atleast 8 characters'],
-    type: String,
-    required: true
-  },
+	name: {
+		type: String,
+		minlength: [3,'Name must have atleast 3 characters'],
+		required: true,
+		unique: true,
+	},
+	number: {
+		minlength: [8,'Number must have atleast 8 characters'],
+		type: String,
+		required: true
+	},
 })
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
 })
 
 module.exports = mongoose.model('Person', personSchema)
